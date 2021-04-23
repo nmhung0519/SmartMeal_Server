361942fd9b426518e3cbf148eaeff2f3283744ef
Mạnh Hung <nmhung0519@gmail.com>
2021-04-23 15:36:46 +0700

    Create Project

---
+++ SmartMeal_Server/Hubs/TestMoveHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartMeal_Server.Hubs
+{
+    public class TestMoveHub:Hub
+    {
+        public async Task MoveViewFromServer(float newX, float newY)
+        {
+            Console.WriteLine("Receive position from Server app: " + newX + "/" + newY);
+            await Clients.Others.SendAsync("ReceiveNewPosition", newX, newY);
+        }
+    }
+}
